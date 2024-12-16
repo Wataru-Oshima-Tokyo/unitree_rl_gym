d505ef87a26faae026e0f8333982a4d3d39ee5c6
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2024-12-16 16:05:47 +0900

    adjusted aliengo config and added rough go2

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -2,6 +2,7 @@ from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 
 
 from legged_gym.envs.go2.go2_config import GO2FlatCfg, GO2FlatCfgPPO
+from legged_gym.envs.go2.go2_config import GO2RoughCfg, GO2RoughCfgPPO
 from legged_gym.envs.aliengo.aliengo_config import ALIENGOFlatCfg, ALIENGOFlatCfgPPO
 from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
 from legged_gym.envs.h1.h1_env import H1Robot
@@ -15,6 +16,7 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register("aliengo", LeggedRobot, ALIENGOFlatCfg(), ALIENGOFlatCfgPPO())
 task_registry.register("go2", LeggedRobot, GO2FlatCfg(), GO2FlatCfgPPO())
+task_registry.register("rough_go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
 task_registry.register("h1", H1Robot, H1RoughCfg(), H1RoughCfgPPO())
 task_registry.register("h1_2", H1_2Robot, H1_2RoughCfg(), H1_2RoughCfgPPO())
 task_registry.register("g1", G1Robot, G1RoughCfg(), G1RoughCfgPPO())
--- legged_gym/envs/aliengo/aliengo_config.py
+++ legged_gym/envs/aliengo/aliengo_config.py
@@ -2,29 +2,29 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class ALIENGOFlatCfg( LeggedRobotCfg ):
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.6] # x,y,z [m]
+        pos = [0.0, 0.0, 0.8] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
-            'FL_hip_joint': 0.1,   # [rad]
-            'RL_hip_joint': 0.1,   # [rad]
-            'FR_hip_joint': -0.1 ,  # [rad]
-            'RR_hip_joint': -0.1,   # [rad]
+            'FL_hip_joint': 0.0,   # [rad]
+            'FR_hip_joint': -0.0,  # [rad]
+            'RL_hip_joint': 0.0,   # [rad]
+            'RR_hip_joint': -0.0,   # [rad]
 
-            'FL_thigh_joint': 0.8,     # [rad]
-            'RL_thigh_joint': 1.,   # [rad]
-            'FR_thigh_joint': 0.8,     # [rad]
-            'RR_thigh_joint': 1.,   # [rad]
+            'FL_thigh_joint': 1.6,     # [rad]
+            'FR_thigh_joint': 1.6,     # [rad]
+            'RL_thigh_joint': 1.8,   # [rad]
+            'RR_thigh_joint': 1.8,   # [rad]
 
-            'FL_calf_joint': -1.0,   # [rad]
-            'RL_calf_joint': -1.0,    # [rad]
-            'FR_calf_joint': -1.0,  # [rad]
-            'RR_calf_joint': -1.0,    # [rad]
+            'FL_calf_joint': -1.35,   # [rad]
+            'FR_calf_joint': -1.35,  # [rad]
+            'RL_calf_joint': -1.2,    # [rad]
+            'RR_calf_joint': -1.2,    # [rad]
         }
 
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
         control_type = 'P'
-        stiffness = {'joint': 100.}  # [N*m/rad]
-        damping = {'joint': 0.5}     # [N*m*s/rad]
+        stiffness = {'calf': 10000.,'hip': 40, 'thigh': 40}  # [N*m/rad]
+        damping = {'calf': 200.0,'hip': 1.0, 'thigh': 1.0}     # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.25
         # decimation: Number of control action updates @ sim DT per policy DT
@@ -35,15 +35,16 @@ class ALIENGOFlatCfg( LeggedRobotCfg ):
         name = "aliengo"
         foot_name = "foot"
         penalize_contacts_on = ["thigh", "calf"]
-        terminate_after_contacts_on = ["base"]
+        terminate_after_contacts_on = ["trunk"]
         self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
   
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
-        base_height_target = 0.5
+        base_height_target = 0.43
         class scales( LeggedRobotCfg.rewards.scales ):
-            torques = -0.0002
+            torques = -0.0000001
             dof_pos_limits = -10.0
+            base_height = -10.0
 
 class ALIENGOFlatCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
