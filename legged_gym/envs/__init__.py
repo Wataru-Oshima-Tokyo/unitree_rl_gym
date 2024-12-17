6acfbd311f2da4b8a3aff5ce6fa48332bffa5396
YutoFukuda1113 <yuto.fukuda@techshare.co.jp>
2024-12-17 11:35:24 +0900

    add go1

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -1,6 +1,6 @@
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 
-
+from legged_gym.envs.go1.go1_config import GO1FlatCfg, GO1FlatCfgPPO
 from legged_gym.envs.go2.go2_config import GO2FlatCfg, GO2FlatCfgPPO
 from legged_gym.envs.aliengo.aliengo_config import ALIENGOFlatCfg, ALIENGOFlatCfgPPO
 from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
@@ -14,6 +14,7 @@ from .base.legged_robot import LeggedRobot
 from legged_gym.utils.task_registry import task_registry
 
 task_registry.register("aliengo", LeggedRobot, ALIENGOFlatCfg(), ALIENGOFlatCfgPPO())
+task_registry.register("go1", LeggedRobot, GO1FlatCfg(), GO1FlatCfgPPO())
 task_registry.register("go2", LeggedRobot, GO2FlatCfg(), GO2FlatCfgPPO())
 task_registry.register("h1", H1Robot, H1RoughCfg(), H1RoughCfgPPO())
 task_registry.register("h1_2", H1_2Robot, H1_2RoughCfg(), H1_2RoughCfgPPO())
---
+++ legged_gym/envs/go1/go1_config.py
@@ -0,0 +1,63 @@
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class GO1FlatCfg( LeggedRobotCfg ):
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.35] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.1,   # [rad]
+            'RL_hip_joint': 0.1,   # [rad]
+            'FR_hip_joint': -0.1 ,  # [rad]
+            'RR_hip_joint': -0.1,   # [rad]
+
+            'FL_thigh_joint': 0.8,     # [rad]
+            'RL_thigh_joint': 1.,   # [rad]
+            'FR_thigh_joint': 0.8,     # [rad]
+            'RR_thigh_joint': 1.,   # [rad]
+
+            'FL_calf_joint': -1.5,   # [rad]
+            'RL_calf_joint': -1.5,    # [rad]
+            'FR_calf_joint': -1.5,  # [rad]
+            'RR_calf_joint': -1.5,    # [rad]
+        }
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 20.}  # [N*m/rad]
+        # stiffness = {'hip': 25., 'thigh': 25., 'calf': 25.}  # [N*m/rad]
+        # stiffness = {'joint': 100.}  # [N*m/rad]
+        damping = {'joint': 0.5}     # [N*m*s/rad]
+        # damping = {'hip': 0.5, 'thigh': 0.5, 'calf': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go1/urdf/go1.urdf'
+        name = "go1"
+        foot_name = "foot"
+        # penalize_contacts_on = ["thigh", "calf"]
+        penalize_contacts_on = ["thigh"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+  
+    class rewards( LeggedRobotCfg.rewards ):
+        soft_dof_pos_limit = 0.9
+        base_height_target = 0.25
+        # only_positive_rewards = False
+        class scales( LeggedRobotCfg.rewards.scales ):
+            torques = -0.0002
+            dof_pos_limits = -10.0
+            # base_height = -10.0
+
+class GO1FlatCfgPPO( LeggedRobotCfgPPO ):
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        entropy_coef = 0.01
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = ''
+        # max_iterations = 10000
+        # save_interval = 100
+        experiment_name = 'go1'
+
+  
